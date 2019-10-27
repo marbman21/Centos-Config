7d4e8ba2aa8491d0bdcb7fda9d5938cb62ae1a65
Diyar Abbas <30482423+diyarit@users.noreply.github.com>
2019-10-27 03:20:36 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# CentOS post-installation script.
+
+## Installation
+
+```bash
+wget https://raw.githubusercontent.com/diyarit/Centos-Config/master/configure_centos.sh && bash configure_centos.sh
+```
+
---
+++ configure_centos.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SSH_PORT=2022
+
+if [ ! -f /etc/redhat-release ]; then
+	echo "CentOS not detected, aborting."
+	exit 0
+fi
+
+echo "Updating OS..."
+yum update -y
+yum groupinstall "Base" --skip-broken -y
+yum install screen -y
+sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/sysconfig/selinux
+sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+/usr/sbin/setenforce 0
+iptables-save > /root/firewall.rules
+
+echo "Configuring Network..."
+find /etc/sysconfig/network-scripts/ -name "ifcfg-*" -not -name "ifcfg-lo" | while read ETHCFG
+do
+	sed -i '/^PEERDNS=.*/d' $ETHCFG
+	sed -i '/^DNS1=.*/d' $ETHCFG
+	sed -i '/^DNS2=.*/d' $ETHCFG
+	
+	echo "PEERDNS=no" >> $ETHCFG
+	echo "DNS1=8.8.8.8" >> $ETHCFG
+	echo "DNS2=8.8.4.4" >> $ETHCFG
+
+done
+
+echo "Rewriting /etc/resolv.conf..."
+
+echo "nameserver 8.8.8.8" > /etc/resolv.conf # Google
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf # Google
+
+
+echo "Configuring SSH..."
+sed -i 's/^X11Forwarding.*/X11Forwarding no/' /etc/ssh/sshd_config
+sed -i 's/#UseDNS.*/UseDNS no/' /etc/ssh/sshd_config
+
+echo "Changing SSH port..."
+if [ -d /etc/csf ]; then
+	echo "Opening CSF..."
+        CURR_CSF_IN=$(grep "^TCP_IN" /etc/csf/csf.conf | cut -d'=' -f2 | sed 's/\ //g' | sed 's/\"//g' | sed "s/,$SSH_PORT,/,/g" | sed "s/,$SSH_PORT//g" | sed "s/$SSH_PORT,//g" | sed "s/,,//g")
+        sed -i "s/^TCP_IN.*/TCP_IN = \"$CURR_CSF_IN,$SSH_PORT\"/" /etc/csf/csf.conf
+        csf -r
+fi
+
+echo "Changing SSH portdefault 22 a $SSH_PORT..."
+sed -i "s/^\(#\|\)Port.*/Port $SSH_PORT/" /etc/ssh/sshd_config
+
+service sshd restart
+
+# FIREWALL
+
+# IF YOU HAVE ONLY IPTABLES
+if [ -f /etc/sysconfig/iptables ]; then
+	sed -i 's/dport 22 /dport 2022 /' /etc/sysconfig/iptables
+	service iptables restart 2>/dev/null
+fi
+
+# IF YOU HAVE FIREWALLD
+if systemctl is-enabled firewalld | grep "^enabled$" > /dev/null; then
+	if systemctl is-active firewalld | grep "^inactive$" > /dev/null; then
+		service firewalld restart
+	fi
+	firewall-cmd --permanent --add-port=2022/tcp > /dev/null
+	firewall-offline-cmd --add-port=2022/tcp > /dev/null
+	firewall-cmd --reload 
+fi
+
+echo "Configuring FSCK..."
+grubby --update-kernel=ALL --args=fsck.repair=yes
+grep "fsck.repair" /etc/default/grub > /dev/null || sed 's/^GRUB_CMDLINE_LINUX="/&fsck.repair=yes /' /etc/default/grub
+
+echo "Configuring Yum-Cron..."
+yum -y install yum-cron
+sed -i 's/^apply_updates.*/apply_updates = yes/' /etc/yum/yum-cron.conf
+systemctl start yum-cron.service
+
+echo "Configuring SSD (if owning)..."
+for DEVFULL in /dev/sg? /dev/sd?; do
+	DEV=$(echo "$DEVFULL" | cut -d'/' -f3)
+        if [ -f "/sys/block/$DEV/queue/rotational" ]; then
+        	TYPE=$(grep "0" /sys/block/$DEV/queue/rotational > /dev/null && echo "SSD" || echo "HDD")
+		if [ "$TYPE" = "SSD" ]; then
+			systemctl enable fstrim.timer
+
+		fi
+        fi
+done
+
+echo "Synchronizing date withpool.ntp.org..."
+ntpdate 0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org 0.south-america.pool.ntp.org
+if [ -f /usr/share/zoneinfo/America/New_York ]; then
+        echo "Seteando timezone a America/New_York..."
+        mv /etc/localtime /etc/localtime.old
+        ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+fi
+
+echo "Setting date of BIOS..."
+hwclock -r
+
+echo "Installing GIT..."
+yum install git -y
+
+echo "Installing CRON clean from Journal..."
+echo "30 22 * * * root /usr/bin/journalctl --vacuum-time=1d; /usr/sbin/service systemd-journald restart" > /etc/cron.d/clean_journal
+service crond restart
+
+echo "Finalized!"
